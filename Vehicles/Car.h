5e7705b3a1142654435d60c2fb08018a006c53dd
Colin Russell <colinrussell101@gmail.com>
2018-04-15 22:28:40 -0400

    followed tutorial twice up to the point where the description label should show up, but it doesn't

---
+++ Vehicles/Car.h
@@ -0,0 +1,18 @@
+//
+//  Car.h
+//  Vehicles
+//
+//  Created by Colin on 2018-04-15.
+//  Copyright © 2018 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Car : Vehicle
+
+@property (nonatomic, assign) BOOL isConvertible;
+@property (nonatomic, assign) BOOL isHatchback;
+@property (nonatomic, assign) BOOL hasSunroof;
+@property (nonatomic, assign) NSInteger numberOfDoors;
+
+@end
